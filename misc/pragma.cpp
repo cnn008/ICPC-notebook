4e16f635f5296e9dd2cd183f38e5f8b7933f421f
cnn008 <chuongnguyenng@gmail.com>
2024-11-16 22:36:26 +0700

    ff

--- misc/pragma.cpp
+++ misc/pragma.cpp
@@ -7,10 +7,8 @@
 #pragma GCC optimize("Ofast")
 #pragma GCC optimization("unroll-loops, no-stack-protector")
 #pragma GCC target("avx,avx2,fma")
-
 __attribute__((always_inline)) void smin(int &a, int b) {a = a < b ? a : b;}
 __attribute__((always_inline)) void smax(int &a, int b) {a = a > b ? a : b;}
-
 int read(){
 	int res=0;bool bo=false;char c;
 	while (((c=getchar())<'0'||c>'9')&&c!='-');
@@ -19,9 +17,96 @@ int read(){
 	while ((c=getchar())>='0'&&c<='9') res=(res<<3)+(res<<1)+c-'0';
 	return bo?~res+1:res;
 }
-
 mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 int rand(int l, int r){
     assert(l<=r);
     return uniform_int_distribution<int> (l,r)(rng);
 }
+#define IO_FAST 1
+ 
+const int BufSize=1<<21;
+struct FastIO
+{
+	char ibuf[BufSize+10],*p,*e;
+	char obuf[BufSize+10],*q,stk[128];
+ 
+#if IO_FAST
+	FastIO(){q=obuf;}
+	~FastIO(){fwrite(obuf,1,q-obuf,stdout);}
+ 
+	int getChar()
+	{
+		if(p==e)p=ibuf,e=ibuf+fread(ibuf,1,BufSize,stdin);
+		return p==e?EOF:*p++;
+	}
+	void putChar(int c)
+	{
+		if(q-obuf==BufSize)fwrite(obuf,1,BufSize,stdout),q=obuf;
+		*q++=c;
+	}
+#else
+	int getChar(){return getchar();}
+	void putChar(int c){putchar(c);}
+#endif
+ 
+	FastIO& operator>>(char& c)
+	{
+		do{c=getChar();}while(isspace(c));
+		return *this;
+	}
+	FastIO& operator>>(char* s)
+	{
+		*s=' ';
+		while(isspace(*s))*s=getChar();
+		while(!isspace(*s))*++s=getChar();
+		*s=0;
+		return *this;
+	}
+	template<typename T>
+	FastIO& operator>>(T& x)
+	{
+		char c,l;
+		for(c=0;!isdigit(c);c=getChar())l=c;
+		for(x=0;isdigit(c);c=getChar())x=x*10-'0'+c;
+		if(l=='-')x=-x;
+		return *this;
+	}
+ 
+	FastIO& operator<<(char c)
+	{
+		putChar(c);
+		return *this;
+	}
+	FastIO& operator<<(char* s)
+	{
+		while(*s)putChar(*s++);
+		return *this;
+	}
+	FastIO& operator<<(const char* s)
+	{
+		while(*s)putChar(*s++);
+		return *this;
+	}
+	template<typename T>
+	FastIO& operator<<(T x)
+	{
+		int tp=0;
+		if(x<0)x=-x,putChar('-');
+		do{stk[++tp]=x%10,x/=10;}while(x);
+		while(tp)putChar(stk[tp--]+'0');
+		return *this;
+	}
+}IO;
+// IO>>n, IO<<ans
+// stress
+: 100
+
+gen.exe
+brute.exe
+stress.exe
+
+fc /w arya.ans arya.out
+
+if errorlevel 1 (exit)
+
+goto 100
